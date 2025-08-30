696833da5d1662ab86a5ff08f69a2055747ada3e
Buran <93150601+Burankus@users.noreply.github.com>
2025-08-31 01:10:05 +0700

    Adding files

---
+++ resources/resources code/bullet_stats.gd
@@ -0,0 +1,7 @@
+@tool
+extends Resource
+class_name BulletStats
+
+@export var damage : int = 5
+@export var speed : int = 900
+@export var bullet_range : int = 1000
---
+++ resources/resources code/stats.gd
@@ -0,0 +1,8 @@
+@tool
+extends Resource
+class_name Stats
+
+@export var hp : float
+@export var firing_speed : float
+@export var speed : float
+@export var shield : bool
